c9126bdd1b48e781a139ae97ec3c73097690b652
Rafael Cajazeira <147667168+rcajazeira@users.noreply.github.com>
2025-11-07 12:22:46 -0300

    Estrutura inicial: backend básico e pastas

---
+++ backend/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+)
+
+func main() {
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintf(w, "✅ Backend Veritas está ON!")
+    })
+
+    fmt.Println("🚀 Servidor rodando em http://localhost:8080")
+    fmt.Println("Pressione Ctrl+C para parar")
+
+    err := http.ListenAndServe(":8080", nil)
+    if err != nil {
+        fmt.Printf("❌ Erro: %v\n", err)
+    }
+}
